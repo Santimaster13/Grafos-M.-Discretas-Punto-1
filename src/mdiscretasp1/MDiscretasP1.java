de2fd2848836d674fd3c0a52f12883cec3b01aee
Santi Mercado <Santi Mercado@Black-Knight>
2022-03-28 21:00:07 -0500

    Final Build

---
+++ src/mdiscretasp1/MDiscretasP1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mdiscretasp1;
+
+import javafx.scene.paint.Color;
+
+/**
+ *
+ * @author Santi Mercado
+ */
+public class MDiscretasP1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Interface i = new Interface();
+        i.getContentPane().setBackground(java.awt.Color.LIGHT_GRAY);
+        i.setVisible(true);
+        i.Init();
+        
+    }
+    
+}
